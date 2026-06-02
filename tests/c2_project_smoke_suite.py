69c06fdcb55f0c841f8a16dbf63918a594c2d194
Ahmet Firat <ahmetcakmak1697@gmail.com>
2026-06-02 23:11:50 +0300

    Add C2 roadmap tests to project smoke suite

--- tests/c2_project_smoke_suite.py
+++ tests/c2_project_smoke_suite.py
@@ -20,6 +20,7 @@ TEST_MODULES = [
     "tests/test_c2_1_project_state.py",
     "tests/test_c2_1_telegram_project_state.py",
     "tests/test_c2_2_project_summarizer.py",
+    "tests/test_c2_3_roadmap_detector.py",
 ]
 
 
